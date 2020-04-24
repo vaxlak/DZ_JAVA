27474a9a1b5d1945d6ba16ccb909bb6150f20e2b
vaxlak <vaxlak@inbox.ru>
2020-04-24 09:30:19 +0300

    Create Main.java

---
+++ task_1.7/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int radius=scan.nextInt();
+        double L=2*3.14*radius;
+        System.out.println(L);
+    }
+}
