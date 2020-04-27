f6bed8010b2aa58ff6d8f3291a16be3a73b94db4
vaxlak <vaxlak@inbox.ru>
2020-04-27 11:33:37 +0300

    Update Main.java

--- task_2.5/Main.java
+++ task_2.5/Main.java
@@ -8,7 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        double vklad=1000, p=5;
+        double vklad=scan.nextDouble();
+        double p=scan.nextDouble();
         for(int i=0;i<5;i++) {
             vklad+=vklad*p/100;
             System.out.println(vklad);
