324a1317970018c72e337f09cf3cd494730469e0
vaxlak <vaxlak@inbox.ru>
2020-04-28 13:05:32 +0300

    Update Main.java

--- task_4.6/Main.java
+++ task_4.6/Main.java
@@ -22,13 +22,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        int x;
         int y = scan.nextInt();
-        if (y % 400 == 0) System.out.println("Количество дней в году: 366");
-        else if (y % 100 == 0) System.out.println("Количество дней в году: 365");
-        else if (y % 4 == 0) System.out.println("Количество дней в году: 366");
-        else System.out.println("Количество дней в году: 365");
-
-
+        if (y % 400 == 0) x=366;
+        else if (y % 100 == 0) x=365;
+        else if (y % 4 == 0) x=366;
+        else x=365;
+        System.out.println("Количество дней в году: " + x);
+       
     }
+
 }
+
+
