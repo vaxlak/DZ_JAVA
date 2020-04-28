77ef34ef7c5959d97fd1457e306ca760273bfd9e
vaxlak <vaxlak@inbox.ru>
2020-04-28 10:37:39 +0300

    Create Main.java

---
+++ task_4.1/Main.java
@@ -0,0 +1,18 @@
+/*Задача 4.1
+        Написать функцию, которая возвращает минимум из двух чисел.*/
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x=-20, y=-7;
+        System.out.println(min_from_two(x,y));
+
+    }
+
+    static int min_from_two(int a, int b){
+        if(a<b) return a;
+        else return b;
+
+    }
+}
