95774d2aa49a5dd0f8735f78fd0dbedee9c12352
vaxlak <vaxlak@inbox.ru>
2020-05-07 17:55:16 +0300

    Create Main.java

---
+++ task_6.6/Main.java
@@ -0,0 +1,15 @@
+/*
+Перехватить исключение, возникающее при выполнении кода:
+int a = 42 / 0;
+Вывести на экран тип перехваченного исключения.*/
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int a = 42 / 0;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
