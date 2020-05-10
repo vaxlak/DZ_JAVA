63448c1b5c33c1a4528aba3a90e3239691623d40
vaxlak <vaxlak@inbox.ru>
2020-05-11 01:48:12 +0300

    Create Main.java

---
+++ task_7.7/Main.java
@@ -0,0 +1,14 @@
+
+/*Добавить одну операцию по преобразованию типа, чтобы получался ответ: b = 0
+float f = (float)128.50;
+int i = (int)f;
+int b = (int)(i + f); */
+
+public class Main {
+    public static void main(String[] args) {
+        float f = (float) 128.50;
+        int i = (int) f;
+        int b = (byte) (i + f);
+        System.out.println(b);
+    }
+}
