d15ddb43bb57b97c53401aac2c8f64461d95bf5a
vaxlak <vaxlak@inbox.ru>
2020-06-12 14:32:41 +0300

    Create Question.java

---
+++ opros/Question.java
@@ -0,0 +1,29 @@
+package com.example.start;
+
+import android.widget.TextView;
+
+public class Question {
+    private int textResId;
+    private boolean answerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        this.textResId = textResId;
+        this.answerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return textResId;
+    }
+
+    public void setTextResId(int textResId) {
+        this.textResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return answerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        this.answerTrue = answerTrue;
+    }
+}
